159ac491295190a1e0f19652172ac06cf888b29c
hajdarevicharis <haris.hajdarevic@stu.ibu.edu.ba>
2023-10-12 16:26:17 +0200

    Lab 1 - Q1

--- src/main/java/lab1.java
+++ src/main/java/lab1.java
@@ -6,7 +6,16 @@ public class lab1 {
 
         // first question
         Scanner scanner = new Scanner(System.in);
-
+        String password = "haki123";
+        System.out.println("Please enter a string; ");
+        String userInput = scanner.nextLine();
+        if(userInput.equals(password)) {
+            System.out.println("You entered a correct password!");
+            System.out.println("The secret is: ibueduba");
+        } else {
+            System.out.println("Wrong!");
+        }
+        scanner.close();
 
         // second question
 
