da134ee852623992fdd6c5d0d8dfee35fe0744aa
hajdarevicharis <haris.hajdarevic@stu.ibu.edu.ba>
2023-10-12 22:56:15 +0200

    LAB1 - Q3

--- src/main/java/lab1.java
+++ src/main/java/lab1.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 
 public class lab1 {
     public static void main(String[] args) {
+        q3();
+    }
 
-        // first question
+    public static void q3() {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
-        int i = 0;
-        while(i < 3) {
-            System.out.println("Enter a number: ");
-            int userInput = Integer.parseInt(scanner.nextLine());
-            i++;
-            sum += userInput;
+        System.out.println("Enter number (Enter 0 to finish): ");
+        while (true) {
+            int read = Integer.parseInt(scanner.nextLine());
+            if (read == 0) {
+                break;
+            }
+
+            sum += read;
         }
-        System.out.println("Your sum is: " + sum);
+        System.out.println("The sum of the entered numbers is: " + sum);
         scanner.close();
-
-        // second question
-
-
     }
 }
