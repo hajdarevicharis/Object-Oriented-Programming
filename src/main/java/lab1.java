16e283d933197337fc64b0d3e2ab24c58d887868
hajdarevicharis <haris.hajdarevic@stu.ibu.edu.ba>
2023-10-12 23:05:31 +0200

    LAB1 - Q4

--- src/main/java/lab1.java
+++ src/main/java/lab1.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class lab1 {
     public static void main(String[] args) {
-        q3();
+        //q3();
+        q4();
     }
 
     public static void q3() {
@@ -21,4 +22,28 @@ public class lab1 {
         System.out.println("The sum of the entered numbers is: " + sum);
         scanner.close();
     }
+
+    public static void q4() {
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter a: ");
+        a = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter b: ");
+        b = Integer.parseInt(scanner.nextLine());
+        System.out.println("First: " + a);
+        System.out.println("Last: " + b);
+        while (a < b) {
+            if(a > b) {
+                break;
+            }
+            System.out.println(a++);
+        }
+
+        scanner.close();
+    }
+
+    public static void q5() {
+
+    }
+
 }
