739c8e36f888e5d43c45cfb79cd2ea0314bf6866
hajdarevicharis <haris.hajdarevic@stu.ibu.edu.ba>
2023-11-08 20:47:20 +0100

    Third task

--- src/main/java/Lab06/lab06main.java
+++ src/main/java/Lab06/lab06main.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 public class lab06main {
     public static void main(String[] args) {
-        int[] values = {1,8,3,4,5,2,7,8,3};
+        int[] values = {1,8,3,4,5,7,8};
         //System.out.println("smallest: " + smallest(values));
-        System.out.println("index of smallest: " + indexOfSmallest(values));
+        //System.out.println("index of smallest: " + indexOfSmallest(values));
+        System.out.println("index of smallest with starting index is: " + indexOfSmallestWithStartingIndex(values, 1));
 
     }
     public static int smallest(int[] array) {
@@ -23,6 +24,17 @@ public class lab06main {
         int smallest = 999;
         int index = 0;
         for(int i = 0; i <= array.length - 1; i++) {
+            if(array[i] <= smallest) {
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static int indexOfSmallestWithStartingIndex(int[] array, int index) {
+        int smallest = 999;
+        for (int i = index; i <= array.length - 1; i++) {
             if(array[i] <= smallest) {
                 smallest = array[i];
                 index = i;
