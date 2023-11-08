cfe9788cb547a16b62a5bb78724b57584c7ce374
hajdarevicharis <haris.hajdarevic@stu.ibu.edu.ba>
2023-11-08 20:40:10 +0100

    second task

--- src/main/java/Lab06/lab06main.java
+++ src/main/java/Lab06/lab06main.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public class lab06main {
     public static void main(String[] args) {
-        int[] values = {8,3,4,5,2,7,8,3};
-        System.out.println("smallest: " + smallest(values));
+        int[] values = {1,8,3,4,5,2,7,8,3};
+        //System.out.println("smallest: " + smallest(values));
+        System.out.println("index of smallest: " + indexOfSmallest(values));
 
     }
     public static int smallest(int[] array) {
@@ -16,5 +17,17 @@ public class lab06main {
             }
         }
         return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int smallest = 999;
+        int index = 0;
+        for(int i = 0; i <= array.length - 1; i++) {
+            if(array[i] <= smallest) {
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
     }
 }
